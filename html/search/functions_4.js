c4224d8485dfb0305bb5ebc8c816c527c8c6f698
igorsergioJS <64499865+igorsergioJS@users.noreply.github.com>
2020-12-16 21:56:15 -0300

    Add files via upload

---
+++ html/search/functions_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['putbox',['PutBox',['../class_put_box.html#a0cb7487b6058969a8a015842cf3257de',1,'PutBox']]],
+  ['putellipsoid',['PutEllipsoid',['../class_put_ellipsoid.html#a4b7bd5b2a9a879f093acf58e2c19055b',1,'PutEllipsoid']]],
+  ['putsphere',['PutSphere',['../class_put_sphere.html#aad7d38e4406ef3cd77c6fd60c0b64e43',1,'PutSphere']]],
+  ['putvoxel',['PutVoxel',['../class_put_voxel.html#aba9a1d522ca6fbbd8c3ec2271c9e5c18',1,'PutVoxel::PutVoxel()'],['../class_sculptor.html#a4bdea3048b419d58e93074060eaa7b52',1,'Sculptor::putVoxel()']]]
+];
